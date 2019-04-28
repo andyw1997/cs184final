be87911a0cecde70341ff09e984a3e55caecbd9d
Sam Zhou <jhgzhou@gmail.com>
2019-04-27 17:58:37 -0700

    merged and testing infra

---
+++ cmake-build-debug/src/CMakeFiles/meshedit.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/samz/Documents/School/Graphics/p2-meshedit-JHGZhou/src/bezierCurve.cpp" "/home/samz/Documents/School/Graphics/p2-meshedit-JHGZhou/cmake-build-debug/src/CMakeFiles/meshedit.dir/bezierCurve.cpp.o"
+  "/home/samz/Documents/School/Graphics/p2-meshedit-JHGZhou/src/bezierPatch.cpp" "/home/samz/Documents/School/Graphics/p2-meshedit-JHGZhou/cmake-build-debug/src/CMakeFiles/meshedit.dir/bezierPatch.cpp.o"
+  "/home/samz/Documents/School/Graphics/p2-meshedit-JHGZhou/src/camera.cpp" "/home/samz/Documents/School/Graphics/p2-meshedit-JHGZhou/cmake-build-debug/src/CMakeFiles/meshedit.dir/camera.cpp.o"
+  "/home/samz/Documents/School/Graphics/p2-meshedit-JHGZhou/src/collada.cpp" "/home/samz/Documents/School/Graphics/p2-meshedit-JHGZhou/cmake-build-debug/src/CMakeFiles/meshedit.dir/collada.cpp.o"
+  "/home/samz/Documents/School/Graphics/p2-meshedit-JHGZhou/src/halfEdgeMesh.cpp" "/home/samz/Documents/School/Graphics/p2-meshedit-JHGZhou/cmake-build-debug/src/CMakeFiles/meshedit.dir/halfEdgeMesh.cpp.o"
+  "/home/samz/Documents/School/Graphics/p2-meshedit-JHGZhou/src/light.cpp" "/home/samz/Documents/School/Graphics/p2-meshedit-JHGZhou/cmake-build-debug/src/CMakeFiles/meshedit.dir/light.cpp.o"
+  "/home/samz/Documents/School/Graphics/p2-meshedit-JHGZhou/src/main.cpp" "/home/samz/Documents/School/Graphics/p2-meshedit-JHGZhou/cmake-build-debug/src/CMakeFiles/meshedit.dir/main.cpp.o"
+  "/home/samz/Documents/School/Graphics/p2-meshedit-JHGZhou/src/material.cpp" "/home/samz/Documents/School/Graphics/p2-meshedit-JHGZhou/cmake-build-debug/src/CMakeFiles/meshedit.dir/material.cpp.o"
+  "/home/samz/Documents/School/Graphics/p2-meshedit-JHGZhou/src/mesh.cpp" "/home/samz/Documents/School/Graphics/p2-meshedit-JHGZhou/cmake-build-debug/src/CMakeFiles/meshedit.dir/mesh.cpp.o"
+  "/home/samz/Documents/School/Graphics/p2-meshedit-JHGZhou/src/meshEdit.cpp" "/home/samz/Documents/School/Graphics/p2-meshedit-JHGZhou/cmake-build-debug/src/CMakeFiles/meshedit.dir/meshEdit.cpp.o"
+  "/home/samz/Documents/School/Graphics/p2-meshedit-JHGZhou/src/png.cpp" "/home/samz/Documents/School/Graphics/p2-meshedit-JHGZhou/cmake-build-debug/src/CMakeFiles/meshedit.dir/png.cpp.o"
+  "/home/samz/Documents/School/Graphics/p2-meshedit-JHGZhou/src/scene.cpp" "/home/samz/Documents/School/Graphics/p2-meshedit-JHGZhou/cmake-build-debug/src/CMakeFiles/meshedit.dir/scene.cpp.o"
+  "/home/samz/Documents/School/Graphics/p2-meshedit-JHGZhou/src/student_code.cpp" "/home/samz/Documents/School/Graphics/p2-meshedit-JHGZhou/cmake-build-debug/src/CMakeFiles/meshedit.dir/student_code.cpp.o"
+  "/home/samz/Documents/School/Graphics/p2-meshedit-JHGZhou/src/texture.cpp" "/home/samz/Documents/School/Graphics/p2-meshedit-JHGZhou/cmake-build-debug/src/CMakeFiles/meshedit.dir/texture.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../CGL/include"
+  "/usr/include/freetype2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/samz/Documents/School/Graphics/p2-meshedit-JHGZhou/cmake-build-debug/CGL/src/CMakeFiles/CGL.dir/DependInfo.cmake"
+  "/home/samz/Documents/School/Graphics/p2-meshedit-JHGZhou/cmake-build-debug/CGL/deps/glew/CMakeFiles/glew.dir/DependInfo.cmake"
+  "/home/samz/Documents/School/Graphics/p2-meshedit-JHGZhou/cmake-build-debug/CGL/deps/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- writeGraph.py
+++ writeGraph.py
@@ -1,49 +1,132 @@
+import sys
+
 class Point:
-	def __init__(self, x, y, z, num):
-		self.id = num
-		self.pos = (x, y, z)
-		self.neighbors = set([])
+    def __init__(self, x, y, z, num):
+        self.id = num
+        self.pos = (x, y, z)
+        self.neighbors = set([])
+
+def read_dae(filename, array_name = "Geometry-Mesh-positions-array"):
+    points = []
+    with open(filename) as file:
+        start = False
+        count = 0
+        for line in file:
+            if start:
+                if "float_array" in line:
+                    start = False
+                    break
+
+                nums = line.split()
+                if len(nums) != 3:
+                    print("help sir this line doesn't have 3 floats :(")
+                points.append(Point(float(nums[0]), float(nums[1]), float(nums[2]), count))
+                count += 1
+                
+            elif "<float_array id=\"{}\"".format(array_name) in line:
+                start = True
+
+    return points
+
+def createGraph(points, filename):
+    poly_points = []
+    with open(filename) as file:
+        start = False
+        count = 0
+        for line in file:
+            if "<p>" in line:
+                data = line.split()
+
+                poly_points.append(int(data[0][3:]))
+
+                for i in range(2, len(data), 2):
+                    poly_points.append(int(data[i]))
+
+  
+    print(len(poly_points) % 3)
+    print(len(points))
+    for i in range(0, len(poly_points), 3):
+        print(poly_points[i:i+3])
+        points[poly_points[i]].neighbors.add(poly_points[i + 1])
+        points[poly_points[i]].neighbors.add(poly_points[i + 2])
+
+        points[poly_points[i + 1]].neighbors.add(poly_points[i])
+        points[poly_points[i + 1]].neighbors.add(poly_points[i + 2])
+
+        points[poly_points[i + 2]].neighbors.add(poly_points[i + 1])
+        points[poly_points[i + 2]].neighbors.add(poly_points[i])
+
 
 def genMeshList(pointMap):
-	usedPoints = set([])
-	meshList = []
-	count = 0
-	for num, point in pointMap.items():
-		first = num
-		usedNeighbors = set([])
-		for neighbor in point.neighbors:
-			second = neighbor
-			for secondNeighbor in neighbor.neighbors:
-				if secondNeighbor in point.neighbors and secondNeighbor not in usedNeighbors:
-					if not (first in usedPoints or second in usedPoints or secondNeighbor in usedPoints):
-						meshList.append(first)
-						meshList.append(0)
-						meshList.append(second)
-						meshList.append(0)
-						meshList.append(third)
-						meshList.append(0)
-						count += 1
-			usedNeighbors.add(neighbor)
-		usedPoints.add(num)
+    usedPoints = set([])
+    meshList = []
+    edges = set([])
+    count = 0
+    for num in range(len(pointMap)):
+        point = pointMap[num]
+        first = num
+        usedNeighbors = set([])
+        for neighbor in point.neighbors:
+            second = neighbor
+            for secondNeighbor in pointMap[neighbor].neighbors:
+                if secondNeighbor in point.neighbors and secondNeighbor not in usedNeighbors:
+                    if not (first in usedPoints or second in usedPoints or secondNeighbor in usedPoints):
+                        edge1 = (first, second)
+                        edge2 = (second, secondNeighbor)
+                        edge3 = (secondNeighbor, first)
+                        if not(edge1 in edges or edge2 in edges or edge3 in edges):
+                            meshList.append(first)
+                            meshList.append(first)
+                            meshList.append(second)
+                            meshList.append(second)
+                            meshList.append(secondNeighbor)
+                            meshList.append(secondNeighbor)
+                            print(first, second, secondNeighbor)
+                            edges.add(edge1)
+                            edges.add(edge2)
+                            edges.add(edge3)
+                        else:
+                            meshList.append(secondNeighbor)m, 
+                            meshList.append(secondNeighbor)
+                            meshList.append(second)
+                            meshList.append(second)
+                            meshList.append(first)
+                            meshList.append(first)
+                            print(secondNeighbor, second, first)
+                            edges.add((first, secondNeighbor))
+                            edges.add((secondNeighbor, second))
+                            edges.add((second, first))
+                        count += 1
+            usedNeighbors.add(neighbor)
+        usedPoints.add(num)
 
-	return meshList, count
+    return meshList, count
 
 def writeMeshList(meshList, count, inFile, outFile):
-	orig = open(inFile, "r")
-	mod = open(outFile, "w+")
+    orig = open(inFile, "r")
+    mod = open(outFile, "w+")
 
-	for line in orig:
-		if "<polylist" in line:
-			mod.write('<polylist material="Material-Default" count="' + str(count) + '">\n')
-		elif "<vcount>" in line:
-			mod.write('<vcount>')
-			for i in range(count):
-				mod.write('3 ')
-			mod.write('</vcount>\n')
-		elif "<p>" in line:
-			mod.write('<p>')
-			for vertex in meshList:
-				mod.write(str(vertex) + " ")
-			mod.write('</p>\n')
-		else:
-			mod.write(line)
+    for line in orig:
+        if "<polylist" in line:
+            mod.write('<polylist material="Material-Default" count="' + str(count) + '">\n')
+        elif "<vcount>" in line:
+            mod.write('<vcount>')
+            for i in range(count):
+                mod.write('3 ')
+            mod.write('</vcount>\n')
+        elif "<p>" in line:
+            mod.write('<p>')
+            for vertex in meshList:
+                mod.write(str(vertex) + " ")
+            mod.write('</p>\n')
+        else:
+            mod.write(line)
+
+if __name__ == '__main__':
+    inputFile = sys.argv[1]
+    outputFile = sys.argv[2]
+
+    pointMap = read_dae(inputFile, "Geometry-mesh001-positions-array")
+    createGraph(pointMap, inputFile)
+    meshList, count = genMeshList(pointMap)
+    writeMeshList(meshList, count, inputFile, outputFile)
